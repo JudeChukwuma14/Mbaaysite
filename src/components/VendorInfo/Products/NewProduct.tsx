8e5fbd8cd6db5f6922b1f9ed84b779c72c8be749
johnbosco35 <okonkwovincent63@gmail.com>
2025-02-05 21:58:30 +0100

    added

--- src/components/VendorInfo/Products/NewProduct.tsx
+++ src/components/VendorInfo/Products/NewProduct.tsx
@@ -3,16 +3,22 @@ import { motion } from "framer-motion";
 import { FiUploadCloud } from "react-icons/fi";
 import { CiVideoOn } from "react-icons/ci";
 import { FaYoutube } from "react-icons/fa";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const NewProduct = () => {
   const [productName, setProductName] = useState("");
-  const [productDescription, setProductDescription] = useState("");
+  // const [productDescription, setProductDescription] = useState("");
   const [category, setCategory] = useState("Fashion");
   const [subCategory, setSubCategory] = useState("Men's Wears");
   const [quantity, setQuantity] = useState("0");
   const [sku, setSku] = useState("");
   const [price, setPrice] = useState("");
   const [comparePrice, setComparePrice] = useState("");
+  const [value, setValue] = useState("");
+  
+  
+  // const toolbarOptions = [['bold', 'italic'], ['link', 'image']];
 
   const handleDiscard = () => {
     console.log("Discard remove.");
@@ -50,12 +56,13 @@ const NewProduct = () => {
             value={productName}
             onChange={(e) => setProductName(e.target.value)}
           />
-          <textarea
+          <ReactQuill theme="snow"  placeholder="Product Description"  value={value} onChange={setValue} className="border outline-orange-500 border-orange-500"/>
+          {/* <textarea
             placeholder="Product Description"
             className="w-full p-2 border rounded h-32 outline-orange-500 border-orange-500"
             value={productDescription}
             onChange={(e) => setProductDescription(e.target.value)}
-          />
+          /> */}
         </motion.div>
 
         {/* Product Images Section */}
